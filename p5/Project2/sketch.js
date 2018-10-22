4cacd76504f99a1c248daf32ca75991894b63175
GuardianZin.github.io <ireams56@gmail.com>
2018-10-21 20:19:14 -0700

    update

--- p5/Project2/sketch.js
+++ p5/Project2/sketch.js
@@ -5,10 +5,12 @@ setup(){
 function draw(){
 if(mouseIsPressed) {
   rect(mouseX,mouseY,80,80);
+  ellipse(mouseX,mouseY,-80,-180);
+  fill(255,255,255,0)
   strokeWeight(3);
-fill(0,255,0,100,-100);
+fill(268,118,200,200,);
 } else{
-  fill(255,255,0 -1000000);
+  fill(0,255,0 -0);
 } 
 rect(mouseX,mouseY,-80,-80);
 translate(width /2, height /2);
