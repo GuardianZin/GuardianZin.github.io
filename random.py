a04c668e29c521584da66ef8d703d69414e7ec87
GuardianZin.github.io <ireams56@gmail.com>
2018-11-16 15:17:14 -0800

    add man hang

--- random.py
+++ random.py
@@ -1,35 +1,97 @@
-"""Hangman
-Standard game of Hangman. A word is chosen at random from a list and the
-user must guess the word letter by letter before running out of attempts."""
-
+import time
 import random
 
-def main():
-    welcome = ['Welcome to Hangman! A word will be chosen at random and',
-               'you must try to guess the word correctly letter by letter',
-               'before you run out of attempts. Good luck!'
-               ]
+#welcoming the user
+def read_file():
+  f = open("wordlist.txt")
+  words = f.read()
+  words = words.split("\n")
+  return words
 
-    for line in welcome:
-        print(line, sep='\n')
+def get_a_word(words, min_len=4, max_len=7):
+  word = random.choice(words)
+  while len(words) < min_len or len(word) > max_len:
+    word = random.choice(words)
+    
+    
+name = input("What is your name? ")
 
-    # setting up the play_again loop
+print ("Hello, " + name, "Time to play hangman!")
 
-    play_again = True
+print ("")
 
-    while play_again:
-        # set up the game loop
+#wait for 1 second
 
-        words = ["hangman", "chairs", "backpack", "bodywash", "clothing",
-                 "computer", "python", "program", "glasses", "sweatshirt",
-                 "sweatpants", "mattress", "friends", "clocks", "biology",
-                 "algebra", "suitcase", "knives", "ninjas", "shampoo"
-                 ]
 
-        chosen_word = random.choice(words).lower()
-        player_guess = None # will hold the players guess
-        guessed_letters = [] # a list of letters guessed so far
-        word_guessed = []
-        for letter in chosen_word:
-            word_guessed.append("-") # create an unguessed, blank version of the word
-        joined_word = None # joins the words in the list word_guessed
+print ("Start guessing...")
+
+
+#here we set the secret
+get_a_word(words)
+word = get_a_word(words)
+
+#creates an variable with an empty value
+guesses = ''
+
+#determine the number of turns
+turns = 10
+
+# Create a while loop
+
+#check if the turns are more than zero
+while turns > 0:         
+
+    # make a counter that starts with zero
+    failed = 0             
+
+    # for every character in secret_word    
+    for char in word:      
+
+    # see if the character is in the players guess
+        if char in guesses:    
+    
+        # print then out the character
+            print (char)    
+
+        else:
+    
+        # if not found, print a dash
+            print ("_")     
+       
+        # and increase the failed counter with one
+            failed += 1    
+
+    # if failed is equal to zero
+
+    # print You Won
+    if failed == 0:        
+        print ("You won") 
+
+    # exit the script
+        break              
+
+    print
+
+    # ask the user go guess a character
+    guess = input("guess a character:") 
+
+    # set the players guess to guesses
+    guesses += guess                    
+
+    # if the guess is not found in the secret word
+    if guess not in word:  
+ 
+     # turns counter decreases with 1 (now 9)
+        turns -= 1        
+ 
+    # print wrong
+        print ("Wrong")    
+ 
+    # how many turns are left
+        print ("You have", + turns, 'more guesses') 
+ 
+    # if the turns are equal to zero
+        if turns == 0:           
+    
+        # print "You Loose"
+            print ("You Loose")
